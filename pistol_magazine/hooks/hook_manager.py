c796d58f82883162773520401e94ef04cb73d451
Ealyn <miyuk1@126.com>
2024-06-17 15:23:08 +0800

    checkpoint at 2024-06-17T15:23:08+0800

--- pistol_magazine/hooks/hook_manager.py
+++ pistol_magazine/hooks/hook_manager.py
@@ -1,7 +1,7 @@
 class HookManager:
     def __init__(self):
         """
-        before_generate: Executes operations before generating all data. Suitable for tasks like logging or starting external services.
+        pre_generate: Executes operations before generating all data. Suitable for tasks like logging or starting external services.
         after_generate: Executes operations after generating each data entry but before final processing. Suitable for tasks like data validation or conditional modifications.
         final_generate: Executes operations after generating and processing all data entries. Suitable for final data processing, sending data to message queues, or performing statistical analysis.
         """
